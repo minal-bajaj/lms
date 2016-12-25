51021669d0f6d3fee0a018ac08451e8ddc750690
Minal <Minal@192.168.2.6>
2016-12-25 14:00:11 +0530

    Changes for lms

--- src/main/java/com/lms/app/data/subscription/Address.java
+++ src/main/java/com/lms/app/data/subscription/Address.java
@@ -10,20 +10,49 @@ package com.lms.app.data.subscription;
  * @author Minal
  */
 public class Address {
+    private final String city;
+    private final String country;
     private final String firstName;
     private final String fullName;
     private final String lastName;
+    private final String phone;
+    private final String state;
+    private final String street1;
+    private final String street2;
+    private final String zip;
 
     public Address() {
+        this.city = null;
+        this.country = null;
         this.firstName = null;
         this.fullName = null;
         this.lastName = null;
+        this.phone = null;
+        this.state = null;
+        this.street1 = null;
+        this.street2 = null;
+        this.zip = null;
     }
 
-    public Address(String firstName, String fullName, String lastName) {
+    public Address(String city, String country, String firstName, String fullName, String lastName, String phone, String state, String street1, String street2, String zip) {
+        this.city = city;
+        this.country = country;
         this.firstName = firstName;
         this.fullName = fullName;
         this.lastName = lastName;
+        this.phone = phone;
+        this.state = state;
+        this.street1 = street1;
+        this.street2 = street2;
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
     }
 
     public String getFirstName() {
@@ -36,5 +65,25 @@ public class Address {
 
     public String getLastName() {
         return lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getStreet1() {
+        return street1;
+    }
+
+    public String getStreet2() {
+        return street2;
+    }
+
+    public String getZip() {
+        return zip;
     }
 }
